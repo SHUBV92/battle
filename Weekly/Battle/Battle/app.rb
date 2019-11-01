b41335078238e5d1269ee5a44e996eabf65dc328
your Shubinder Virk <shubinder92@gmail.co.uk>
2019-11-01 13:56:03 +0000

    Attacking Player

--- Weekly/Battle/Battle/app.rb
+++ Weekly/Battle/Battle/app.rb
@@ -1,15 +1,53 @@
 require 'sinatra/base'
-require 'capybara'
-require 'capybara/rspec'
-require 'rspec'
-require 'shotgun'
 
 
 
 class Battle < Sinatra::Base 
+   enable :sessions
+
     get '/' do 
-        "Hello Battle!"
+        erb :index
     end 
 
+    post '/names' do 
+        session[:player_1_name] = params[:player_1_name]
+        session[:player_2_name] = params[:player_2_name]
+        redirect '/play'    
+    end
+
+
+    get '/play/' do 
+        @player_1_name = session[:player_1_name] 
+        @player_2_name = session[:player_2_name]    
+        erb :play
+    end 
+
+    get 'attack' do
+        @player_1_name = session[:player_1_name] 
+        @player_2_name = session[:player_2_name]    
+        erb :attack
+
+    # start the server if ruby file executed directory 
     run! if app_file == $0
 end 
+
+
+
+
+
+
+
+
+
+
+
+# <form action="/">
+
+
+# </form>
+
+# p params 
+# @name = params[:player_1]
+# @name = params[:player_2]
+
+#         erb:index 
--- Weekly/Battle/Battle/spec/spec_helper.rb
+++ Weekly/Battle/Battle/spec/spec_helper.rb
@@ -1,10 +1,13 @@
 ENV['RACK_ENV'] = 'test'
 
-require File.join(File.dirname(app.rb), '..', 'app.rb')
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require './app.rb'
+require 'features/web_helpers'
+
 
 Capybara.app = Battle
 
